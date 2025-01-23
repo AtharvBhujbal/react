8a376992cc82bce8fd5c95f1e81d4facfd117842
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2025-01-23 16:31:05 +0530

    Class and functional components

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// Functional Component
+export const Greet = () => <h1>Hello Atharv</h1> // This is a JSX code
+
+// It can also be written as:
+// export const Greet = () => {
+//     return React.createElement('h1',null,'Hello Atharv from without jsx')
+// }
+
+
+// Another way to export
+// export default Greet
---
+++ hello-world/src/components/Welcome.js
@@ -0,0 +1,8 @@
+import React,{Component} from "react";
+
+// Class Component
+export class Welcome extends Component{
+    render(){
+        return <h1>Welcome Atharv from Class components</h1>
+    }
+}
