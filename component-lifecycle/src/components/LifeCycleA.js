f4f83bcf0425f8148b2c6f29451a2f5a6c2cd0ee
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2025-04-09 19:27:53 +0530

    Component Update LifeCycle

--- component-lifecycle/src/components/LifeCycleA.js
+++ component-lifecycle/src/components/LifeCycleA.js
@@ -2,26 +2,49 @@ import React, { Component } from 'react'
 import LifeCycleB from './LifeCycleB'
 
 class LifeCycleA extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         name: 'Vishwas'
-      }
-      console.log('LifeCycleA constructor')
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: 'Vishwas'
     }
-    static getDerivedStateFromProps(props, state) {
-        console.log('LifeCycleA getDerivedStateFromProps')
-        return null
-    }
-    componentDidMount() {
-        console.log('LifeCycleA componentDidMount')
-    }
+    console.log('LifeCycleA constructor')
+  }
+  static getDerivedStateFromProps(props, state) {
+    console.log('LifeCycleA getDerivedStateFromProps')
+    return null
+  }
+  componentDidMount() {
+    console.log('LifeCycleA componentDidMount')
+  }
+
+  shouldComponentUpdate(){
+    console.log('LifeCycleA shouldComponentUpdate')
+    return true
+  }
+
+  getSnapshotBeforeUpdate(){
+    console.log('LifeCycleA getSnapshotBeforeUpdate')
+    return null
+  }
+
+  componentDidUpdate(){
+    console.log("LifeCycleA componentDidUpdate")
+  }
+
+  changeState = () =>{
+    this.setState({
+      'name':"Atharv"    
+    })
+  }
+
+  getSnap
   render() {
     console.log('LifeCycleA render')
     return (
         <div>
             <h1>LifeCycleA</h1>
+            <button onClick={this.changeState}>Change Component</button>
             <LifeCycleB />
         </div>
     )
--- component-lifecycle/src/components/LifeCycleB.js
+++ component-lifecycle/src/components/LifeCycleB.js
@@ -16,6 +16,20 @@ class LifeCycleB extends Component {
     componentDidMount() {
         console.log('LifeCycleB componentDidMount')
     }
+
+    shouldComponentUpdate(){
+      console.log('LifeCycleB shouldComponentUpdate')
+      return true
+    }
+
+    getSnapshotBeforeUpdate(){
+      console.log('LifeCycleB getSnapshotBeforeUpdate')
+      return null
+    }
+  
+    componentDidUpdate(){
+      console.log("LifeCycleB componentDidUpdate")
+    }
   render() {
     console.log('LifeCycleB render')
     return (
